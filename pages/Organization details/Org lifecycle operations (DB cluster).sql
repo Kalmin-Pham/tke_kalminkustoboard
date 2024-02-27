bd7c32430da4405fc21568a69625e864a9582af6
Kalmin Pham <160694481+Kalmin-Pham@users.noreply.github.com>
2024-02-27 15:15:12 +0700

    Create Org lifecycle operations (DB cluster).sql

---
+++ pages/Organization details/Org lifecycle operations (DB cluster).sql
@@ -0,0 +1,34 @@
+let cllgetorginfo = (
+union 
+cluster("https://crmanacus.kusto.windows.net").database("CRMAnalytics").OrganizationDetails,
+cluster("https://crmanaweu.kusto.windows.net").database("CRMAnalytics").OrganizationDetails
+| where TIMESTAMP between (_startTime.._endTime)
+| where OrgId == orgid or isempty(orgid)
+| where BAPEnvironmentId == envid or isempty(envid)
+| extend URL = case(
+Geo=='NA', strcat('https://', DomainName, '.crm.dynamics.com'),
+Geo=='LATAM', strcat('https://', DomainName, '.crm2.dynamics.com'),
+Geo=='CAN', strcat('https://', DomainName, '.crm3.dynamics.com'),
+Geo=='EMEA', strcat('https://', DomainName, '.crm4.dynamics.com'),
+Geo=='APAC', strcat('https://', DomainName, '.crm5.dynamics.com'),
+Geo=='OCE', strcat('https://', DomainName, '.crm6.dynamics.com'),
+Geo=='JPN', strcat('https://', DomainName, '.crm7.dynamics.com'),
+Geo=='IND', strcat('https://', DomainName, '.crm8.dynamics.com'),
+Geo=='TIP', strcat('https://', DomainName,'.crm10.dynamics.com'),
+Geo=='GBR', strcat('https://', DomainName, '.crm11.dynamics.com'),
+Geo=='FRA', strcat('https://', DomainName, '.crm12.dynamics.com'),
+Geo=='ZAF', strcat('https://', DomainName, '.crm14.dynamics.com'),
+Geo=='GER', strcat('https://', DomainName, '.crm16.dynamics.com'),
+Geo=='CHE', strcat('https://', DomainName, '.crm17.dynamics.com'),
+Geo=='NOR', strcat('https://', DomainName, '.crm19.dynamics.com'),
+Geo=='SGP', strcat('https://', DomainName, '.crm20.dynamics.com'),
+Geo=='KOR', strcat('https://', DomainName, '.crm21.dynamics.com'),
+Geo=='AUP', 'Deleted or Test cluster', 'Undefined'
+)
+| where DomainName == domainname or URL startswith domainname or URL startswith substring(domainname, 0, indexof(domainname, ".com") + 4) or isempty(domainname)
+| project BAPEnvironmentId
+| take 1
+);
+BaprpEnvironmentLifecycleOperations(TraceEvents, _startTime, _endTime)
+| where sourceEnvironmentName in (cllgetorginfo) or targetEnvironmentName in (cllgetorginfo)
+| project TIMESTAMP, operationType, sourceEnvironmentName, targetEnvironmentName, ActivityId, correlationId, clientRequestId, clientSessionId, triggeredById, message
